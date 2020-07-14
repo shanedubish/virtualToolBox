e78976f22154f7732f44d9d2eeffd0ac4208262f
Shane <shanedubish@gmail.com>
2020-07-13 22:18:49 -0400

    got full crud on backend working and .env with secret key fixed

--- backend/toolbox/toolbox/settings.py
+++ backend/toolbox/toolbox/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&y85p5u165q@5mt-n^lbc1jlt_&3f08-q!de5)87=+)bk&y1)x'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- backend/toolbox/v_toolbox/api/views.py
+++ backend/toolbox/v_toolbox/api/views.py
@@ -1,8 +1,18 @@
-from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListAPIView,RetrieveUpdateDestroyAPIView,CreateAPIView
 
 from v_toolbox.models import ToolBox
 from .serializers import ToolSerializer
 
 class ToolsListView(ListAPIView):
+    queryset = ToolBox.objects.all()
+    serializer_class = ToolSerializer
+
+
+
+class ToolsGetUpdateDelete(RetrieveUpdateDestroyAPIView):
+    queryset = ToolBox.objects.all()
+    serializer_class = ToolSerializer
+
+class ToolsCreate(CreateAPIView):
     queryset = ToolBox.objects.all()
     serializer_class = ToolSerializer
